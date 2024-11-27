2e84b891f9e0cf096283e2ed1051c56333e75a49
mansoor1123456 <mansoorishaq05@gmail.com>
2024-11-28 03:03:53 +0500

    first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,19 @@
+function HeroPage() {
+    return (
+        <>
+            <div className="relative h-screen w-full flex flex-col">
+                {/* Main content */}
+                <div className="flex-grow">
+                    {/* Your main content goes here */}
+                </div>
+
+                {/* Footer */}
+                <div className="py-1 bg-red-600 text-center text-white">
+                    Footer (HomeWork class-3) <br />| Mansoor | GIAIC
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default HeroPage;
